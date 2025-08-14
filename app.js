2b4653eaed95155ad642ad218fd4612abb20d02c
Vickthor <dialman.vic@gmail.com>
2025-08-14 14:41:07 -0600

    Agrega los archivos base del programa

---
+++ app.js
@@ -0,0 +1,13 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let ListaNombres = [];
+let NombreSecreto;
+let NombreGenerado;
+
+function agregarAmigo(NombreGenerado){
+    ListaNombres.includes(NombreGenerado)
+    return NombreGenerado;
+}
+
+console.log(NombreGenerado);
+console.log(agregarAmigo);
+
